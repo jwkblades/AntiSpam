e6fb525e40531faa6c1b29fe46f3846e0a28a267
James <troomps01+betastream@gmail.com>
2017-02-06 20:35:43 -0700

    Less naive approach to blacklisting words.

--- main.cpp
+++ main.cpp
@@ -8,18 +8,16 @@
 using namespace std;
 
 /**
- * Naive approach - substring matching is a bad idea since it doesn't guarantee
- * that they were meaning to be vulgar.
- *
- * "I am late on my assignment."
- *                  ---          
- * Contains a curse word, but it isn't a cursing/ bad sentence.
+ * A less naive approach - look at whole words at a time.
  */
 std::string containsBannedWords(const std::string& input, const std::vector<std::string>& bannedSet)
 {
+	std::vector<std::string> vec = split(input, " ");
+	std::set<std::string> words;
+	words.insert(vec.begin(), vec.end());
 	for (const std::string& bannedWord : bannedSet)
 	{
-		if (input.find(bannedWord) != std::string::npos)
+		if (words.find(bannedWord) != words.end())
 		{
 			return bannedWord;
 		}
@@ -29,7 +27,7 @@ std::string containsBannedWords(const std::string& input, const std::vector<std:
 
 int main(void)
 {
-	std::string in = "Pancakes and waffles.";
+	std::string in = "Pancakes and fudge waffles.";
 	std::vector<std::string> bannedWords = {"fudge", "cake"};
 
 	cout << "The sentence '" << in << "' contains banned word: '" << containsBannedWords(in, bannedWords) << "'" << endl;
