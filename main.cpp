32b8259634057505e6c7e90c3b1d99b63391b819
James <troomps01+betastream@gmail.com>
2017-02-06 20:46:21 -0700

    A simple similarity function to compare strings.

--- main.cpp
+++ main.cpp
@@ -25,12 +25,46 @@ std::string containsBannedWords(const std::string& input, const std::vector<std:
 	return "";
 }
 
+double similarity(const std::string& a, const std::string& b)
+{
+	std::vector<std::string> vec = split(a, " ");
+	std::set<std::string> setA;
+	setA.insert(vec.begin(), vec.end());
+
+	vec = split(b, " ");
+	std::set<std::string> setB;
+	setB.insert(vec.begin(), vec.end());
+
+	std::set<std::string> intersection;
+	std::set<std::string> unionSet;
+
+	for (const std::string& wordA : setA)
+	{
+		if (setB.count(wordA)) // 0 is false, non-0 is true
+		{
+			intersection.insert(wordA);
+		}
+	}
+	unionSet = setA;
+	unionSet.insert(setB.begin(), setB.end());
+
+	double top = intersection.size();
+	double bottom = unionSet.size();
+
+	return top / bottom;
+}
+
 int main(void)
 {
-	std::string in = "Pancakes and fudge waffles.";
+	std::string in1 = "a rose is a rose is a rose";
+	std::string in2 = "And in the morning, I'm making waffles.";
+	std::string in3 = "this is a rose";
 	std::vector<std::string> bannedWords = {"fudge", "cake"};
 
-	cout << "The sentence '" << in << "' contains banned word: '" << containsBannedWords(in, bannedWords) << "'" << endl;
+	cout << "The sentence '" << in1 << "' contains banned word: '" << containsBannedWords(in1, bannedWords) << "'" << endl;
+
+	cout << "The simple similarity between '" << in1 << "' and '" << in2 << "' is " << similarity(in1, in2) << endl;
+	cout << "The simple similarity between '" << in1 << "' and '" << in3 << "' is " << similarity(in1, in3) << endl;
 
 	return 0;
 }
